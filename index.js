8d01232cab4416e9c7d592ce95d91a9403295436
AhmadBreiss <ahmatobreiss@gmail.com>
2023-04-17 23:29:06 +0300

    edited index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
-import productRouter from './Routes/productRoute.js';
+import productRouter from "./Routes/productRoute.js";
 import categoryRouter from "./Routes/categoryRoute.js";
 import userRouter from "./Routes/userRoute.js";
 
@@ -17,11 +17,11 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/product' , productRouter)
-app.use('/category' , categoryRouter)
-app.use('/user' , userRouter)
+app.use("/product", productRouter);
+app.use("/category", categoryRouter);
+app.use("/user", userRouter);
 app.listen(
   PORT,
   console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
