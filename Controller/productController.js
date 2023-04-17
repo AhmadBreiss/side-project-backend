9732f62c2cd896163c53bc66f9ada90a37bc0393
AhmadBreiss <ahmatobreiss@gmail.com>
2023-04-17 23:27:06 +0300

    updated product controller for media upload

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,62 +1,100 @@
-import ProductModel from "../Models/productModel.js"
+import ProductModel from "../Models/productModel.js";
+import fs from "fs";
 
 // ADD a Product
 function addProduct(req, res, next) {
-    try {
-        let data = req.body
-        let ProductData = new ProductModel(data)
-        ProductData.save()
-        res.status(200).json({ response: ProductData })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err })
-    }
+  console.log(req.body);
+  let { path } = req.image || "";
+  let { name, description, price } = req.body;
+  let body = {
+    name: name,
+    description: description,
+    price: price,
+    image: path,
+  };
+  let doc = new ProductModel(body);
+  console.log(doc);
+  doc
+    .save()
+    .then((response) => {
+      res.status(200).send({ success: true, response });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `ERROR ${err}`,
+        success: false,
+      });
+    });
 }
 
 // Get all the data of Product
 async function getProduct(req, res, next) {
-    try {
-        const get = await ProductModel.find({})
-        res.status(200).json({ response: get })
-    } catch (err) {
-        res.status(400).json(err)
-    }
+  try {
+    const get = await ProductModel.find({});
+    res.status(200).json({ response: get });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 }
 
 // Get Product By id
 async function getProductByID(req, res, next) {
-    let id=req.params.id
-    try {
-        const getById = await ProductModel.findById({_id:id})
-        res.status(200).json({ response: getById })
-    } catch (err) {
-        res.status(400).json(err)
-    }
+  let id = req.params.id;
+  try {
+    const getById = await ProductModel.findById({ _id: id });
+    res.status(200).json({ response: getById });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 }
 
 // Delete Product by id
 async function deleteProduct(req, res, next) {
-    let id = req.params.id
-    try {
-        const deleteById = await ProductModel.findByIdAndDelete({ _id: id })
-        res.status(200).json({ message: "Product delete success", response : deleteById})
-    } catch (err) {
-        res.status(400).json(err)
-    }
+  let id = req.params.id;
+  try {
+    const deleteById = await ProductModel.findByIdAndDelete({ _id: id });
+    res
+      .status(200)
+      .json({ message: "Product delete success", response: deleteById });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 }
 
 // Update Product By id
-async function updateProduct(req,res,next){
-    let id = req.params.id
-    let data = req.body
-    try{
-        await ProductModel.updateOne({_id:id , $set:data})
-        let response = await ProductModel.findById({_id:id})
-        res.status(200).json({message : "Update sucss" , response })
-    }catch(err){
-        res.status(400).json(err)
-    }
-}
-const Product = {addProduct ,getProduct, getProductByID ,deleteProduct ,updateProduct}
+async function updateProduct(req, res, next) {
+  let { path } = req.image || "";
+  let { name, description, price } = req.body;
+  let body = {
+    name: name,
+    description: description,
+    price: price,
+    image: path,
+  };
+  let { id } = req.params;
 
-export default Product ;
+  ProductModel.findOneAndUpdate(
+    { _id: id },
+    { $set: body },
+    console.log(req.body)
+  )
+    .then((response) => {
+      if (req.image) fs.unlinkSync(response.image);
+      res.status(200).send({ success: true, response });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `ERROR ${err}`,
+        success: false,
+      });
+    });
+}
+const Product = {
+  addProduct,
+  getProduct,
+  getProductByID,
+  deleteProduct,
+  updateProduct,
+};
+
+export default Product;
