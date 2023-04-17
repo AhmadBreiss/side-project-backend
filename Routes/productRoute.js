905dbc2c36f46318a047969c4140c335e6586b5e
AhmadBreiss <ahmatobreiss@gmail.com>
2023-04-17 23:27:45 +0300

    edited product route for media upload

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import ProductController from "../Controller/productController.js";
+import {upload} from "../Middelwares/mediaUpload.js";
 const router = express.Router();
 
 router.get("/", ProductController.getProduct);
 router.get("/:id", ProductController.getProductByID);
-router.post("/", ProductController.addProduct);
+router.post("/", upload, ProductController.addProduct);
 router.delete("/:id", ProductController.deleteProduct);
-router.put("/:id", ProductController.updateProduct);
-
+router.patch("/:id",upload, ProductController.updateProduct);
 
 export default router;
